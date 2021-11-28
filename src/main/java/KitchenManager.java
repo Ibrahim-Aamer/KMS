06e4b547c108844f0a92b4786ce6c79c5ad49d5a
Ibrahim <i190607@nu.edu.pk>
2021-11-29 00:17:51 +0500

    Initial commit

---
+++ src/main/java/KitchenManager.java
@@ -0,0 +1,20 @@
+public class KitchenManager extends Employee{
+
+    //Default constructor
+    //KitchenManager() {}
+
+    //Parameterized constructor
+    KitchenManager(int ID, String firstName, String lastName)
+    {
+        idCounter++;//incrementing ID counter
+
+        this.ID = idCounter;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //void CreateWorkSchedule(){}
+
+    //void MachineryMaintenance function
+
+}
